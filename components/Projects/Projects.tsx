5ccefd679ff2aa6259fae11a32dfed41633385ae
zztkdfo_m1 <somehow.dev0224@gmail.com>
2024-03-14 20:56:19 +0900

    Feat: 프로젝트 기본 레이아웃 구성

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -22,6 +22,44 @@ export default function Projects() {
             실제로 라이브 서비스 중인 프로젝트, 전시회 프로젝트, 토이 프로젝트
             등등 다양한 기술과 경험으로 만든 프로젝트 입니다.
           </div>
+          <div className={styles.projectYears}>
+            <ul>
+              <li className={styles.active}>전체</li>
+              <li>2024</li>
+              <li>2023</li>
+              <li>2022</li>
+              <li>2021</li>
+            </ul>
+          </div>
+          <div className={styles.projectListWrap}>
+            <div
+              className={styles.projectWrap}
+              style={{
+                minHeight: "450px",
+              }}
+            >
+              <div className={styles.projectTitle}>Project A</div>
+              <div className={styles.projectList}></div>
+            </div>
+            <div
+              className={styles.projectWrap}
+              style={{
+                minHeight: "450px",
+              }}
+            >
+              <div className={styles.projectTitle}>Project B</div>
+              <div className={styles.projectList}></div>
+            </div>
+            <div
+              className={styles.projectWrap}
+              style={{
+                minHeight: "450px",
+              }}
+            >
+              <div className={styles.projectTitle}>Project B</div>
+              <div className={styles.projectList}></div>
+            </div>
+          </div>
         </motion.div>
       </div>
     </div>
