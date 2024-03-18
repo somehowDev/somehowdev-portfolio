caac55d7abf4a45327c4ccea60ea4cc802c784f0
zztkdfo_m1 <somehow.dev0224@gmail.com>
2024-03-18 22:07:25 +0900

    Feat: 프로젝트 데이터화 작업중

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import styles from "./styles.module.css";
 import { slideInFromTop } from "@/utils/motion";
@@ -8,29 +8,82 @@ import { slideInFromTop } from "@/utils/motion";
 const projectsData = [
   {
     year: "2024",
-    title: "Project A",
+    title: "LLM 기반 AI 교육 플랫폼",
+    skills: [],
     description: "A description for Project A.",
+    visible: false,
+    link: "",
+  },
+  {
+    year: "2024",
+    title: "어쩌다 개발자 포트폴리오",
+    skills: [],
+    description: "A description for Project A.",
+    visible: true,
+    link: "",
   },
   {
     year: "2023",
-    title: "Project B",
-    description: "A description for Project B.",
+    title: "DX 플랫폼 - 재고 관리 솔루션",
+    skills: [],
+    description:
+      "운영중인 스타트업 일본 Edge Tech+ 전시회 참석을 위한 DX 플랫폼 개발",
+    visible: false,
+    link: "",
+  },
+  {
+    year: "2023",
+    title: "DX 플랫폼 - 데이터 센터 관리",
+    skills: [],
+    description:
+      "운영중인 스타트업 일본 Edge Tech+ 전시회 참석을 위한 DX 플랫폼 개발",
+    visible: false,
+    link: "",
+  },
+  {
+    year: "2023",
+    title: "DX 플랫폼 - 자산 관리 솔루션",
+    skills: [],
+    description:
+      "운영중인 스타트업 일본 Edge Tech+ 전시회 참석을 위한 DX 플랫폼 개발",
+    visible: false,
+    link: "",
+  },
+  {
+    year: "2023",
+    title: "비밀리 Web 개발",
+    skills: [],
+    description:
+      "비밀리(다날) 패밀리타운(Family Town) - 모바일 메신저 플랫폼 서비스",
+    visible: true,
+    link: "https://www.family-town.com/features",
   },
   {
     year: "2022",
-    title: "Project C",
-    description: "A description for Project C.",
+    title: "공장 DX 플랫폼",
+    skills: [],
+    description:
+      "공압계와 전력을 매핑시켜 데이터를 분석하여 공장 수율 및 자동화 프로세스를 통한 스마트 팩토리 프로젝트",
+    visible: true,
+    link: "",
   },
-  // 여기에 더 많은 프로젝트를 추가할 수 있습니다.
+  {
+    year: "2022",
+    title: "게임 회사 서버실 출입 관리 솔루션",
+    skills: [],
+    description: "유명 게임회사의 서버실 출입에 관한 DX 솔루션",
+    visible: false,
+    link: "",
+  },
 ];
 
 export default function Projects() {
-  const [activeYear, setActiveYear] = useState("전체");
+  const currentYear = new Date().getFullYear(); // 현재 연도 가져오기
+  const [activeYear, setActiveYear] = useState(currentYear.toString());
 
-  const filteredProjects = projectsData.filter((project) =>
-    activeYear === "전체" ? true : project.year === activeYear
+  const filteredProjects = projectsData.filter(
+    (project) => project.year === activeYear
   );
-  console.log(activeYear);
 
   return (
     <div className={styles.container}>
@@ -48,7 +101,12 @@ export default function Projects() {
           </div>
           <div className={styles.projectYears}>
             <ul>
-              {["전체", "2024", "2023", "2022", "2021"].map((year) => (
+              {[
+                currentYear.toString(),
+                (currentYear - 1).toString(),
+                (currentYear - 2).toString(),
+                (currentYear - 3).toString(),
+              ].map((year) => (
                 <li
                   key={year}
                   className={activeYear === year ? styles.active : ""}
@@ -59,14 +117,26 @@ export default function Projects() {
               ))}
             </ul>
           </div>
-          <div className={styles.projectListWrap}>
+          <div
+            className={styles.projectListWrap}
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
+            }}
+          >
             {filteredProjects.map((project, index) => (
               <div
                 key={index}
                 className={styles.projectWrap}
-                style={{ minHeight: "450px" }}
+                // flexBasis 또는 width를 사용하여 각 프로젝트의 기본 크기를 설정
               >
-                <div className={styles.projectTitle}>{project.title}</div>
+                <div className={styles.projectTitle}>
+                  {project.title}
+                  {project.visible === false ? (
+                    <span className={styles.projectVisible}>대외비</span>
+                  ) : null}
+                </div>
                 <div className={styles.projectDescription}>
                   {project.description}
                 </div>
